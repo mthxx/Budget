34d88a35a43327df75564d2befac5d4e0d04b668
Marc Thomas <mat@mthx.org>
2014-09-29 21:48:03 -0500

    Resized + button

--- window.py
+++ window.py
@@ -10,7 +10,12 @@ class Window(Gtk.Window):
         self.hb.set_show_close_button(True)
         self.set_titlebar(self.hb)
 
-
+        # Date Range Buttons
+        self.day_button = Gtk.Button("Day")
+        self.week_button = Gtk.Button("Week")
+        self.month_button = Gtk.Button("Month")
+        
+        # Navigation Buttons
         self.overview_button = Gtk.Button("Overview")
         self.income_button = Gtk.Button("Income")
         self.expenses_button = Gtk.Button("Expenses")
@@ -23,22 +28,33 @@ class Window(Gtk.Window):
         self.reports_button.halign = Gtk.Align.CENTER;
         self.projections_button.halign = Gtk.Align.CENTER;
         
+        # Quick Add Buttons
+        self.quick_add_button = Gtk.Button()
+        self.icon = Gio.ThemedIcon(name="list-add-symbolic")
+        self.image = Gtk.Image.new_from_gicon(self.icon, Gtk.IconSize.MENU)
+        self.quick_add_button.set_size_request(32,32)
+        self.quick_add_button.valign = Gtk.Align.CENTER;
+        self.quick_add_button.add(self.image)
         
+        # Date Range Box
+        self.range_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(self.range_box.get_style_context(), "linked")
+        self.range_box.add(self.day_button)
+        self.range_box.add(self.week_button)
+        self.range_box.add(self.month_button)
         
+        # Navigation Box
         self.nav_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(self.nav_box.get_style_context(), "linked")
-        Gtk.StyleContext.add_class(self.nav_box.get_style_context(), "raised")
         self.nav_box.add(self.overview_button)
         self.nav_box.add(self.income_button)
         self.nav_box.add(self.expenses_button)
         self.nav_box.add(self.reports_button)
         self.nav_box.add(self.projections_button)
-        self.hb.pack_start(self.nav_box)
        
-        self.quick_add_button = Gtk.Button()
-        self.icon = Gio.ThemedIcon(name="list-add-symbolic")
-        self.image = Gtk.Image.new_from_gicon(self.icon, Gtk.IconSize.MENU)
-        self.quick_add_button.add(self.image)
+        # Header Bar Packing
+        self.hb.pack_start(self.range_box)
+        self.hb.pack_start(self.nav_box)
         self.hb.pack_end(self.quick_add_button)
 
 
