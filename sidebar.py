8247569b15c5cee3c5ba7e075d6470d01ea1b200
Marc Thomas <mathomastech@gmail.com>
2014-10-12 11:17:43 -0500

    Removed layout specific code

--- sidebar.py
+++ sidebar.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk, Gio
 
 class Sidebar():
 
-    def __init__(self,menu,subMenu):
+    def __init__(self):
 
         # Create Grid, Sidebar, List, and Buttons
         self.grid = Gtk.Grid()
@@ -18,14 +18,10 @@ class Sidebar():
         self.subMenuViewport = Gtk.Viewport()
         self.contentViewport = Gtk.Viewport()
         
+        self.categoryNotebook = Gtk.Notebook()
+        
         self.menuButtons = []
         self.subMenuButtons = []
-        
-        for i in range(0,len(menu)):
-            self.menuButtons.append(Gtk.Button(menu[i]))
-        
-        for i in range(0,len(subMenu)):
-            self.subMenuButtons.append(Gtk.Button(subMenu[i]))
             
         # Set Styling
         self.menuScrolledWindow.set_vexpand(True)
@@ -34,21 +30,6 @@ class Sidebar():
         self.subMenuScrolledWindow.set_vexpand(True)
         self.subMenuScrolledWindow.set_property("width-request",100)
         
-        for i in range(0,len(menu)):
-            self.menuButtons[i].set_relief(Gtk.ReliefStyle.NONE)
-            self.menuButtons[i].set_property("height-request", 60)
-        
-        for i in range(0,len(subMenu)):
-            self.subMenuButtons[i].set_relief(Gtk.ReliefStyle.NONE)
-            self.subMenuButtons[i].set_property("height-request", 60)
-            
-        # Add buttons to menu 
-        for i in range(0,len(menu)):
-            self.menuListBox.add(self.menuButtons[i])
-        
-        for i in range(0,len(subMenu)):
-            self.subMenuListBox.add(self.subMenuButtons[i])
-            
         # Add items to Grid 
         self.menuViewport.add(self.menuListBox)
         self.subMenuViewport.add(self.subMenuListBox)
@@ -60,3 +41,7 @@ class Sidebar():
         self.grid.attach(self.menuScrolledWindow,0,0,1,1)
         self.grid.attach(self.subMenuScrolledWindow,2,0,1,1)
         self.grid.attach(self.contentScrolledWindow,1,0,1,1)
+
+
+    def menu_clicked(self,button):
+        (button.get_label())
