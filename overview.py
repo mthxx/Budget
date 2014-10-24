b2ed19a2a351fbbabd7fa62a08d17966ae8da0c6
Marc Thomas <mat@mthx.org>
2014-10-24 15:50:19 -0500

    Added more highlight and data selection options

--- overview.py
+++ overview.py
@@ -15,6 +15,10 @@ class Overview():
         self.monthIndex = 10000
         self.categoryIndex = 10000
 
+        self.menuColor = .5
+        self.totalColor = .75
+        self.highlightColor = .1
+
         # Create Layouts
         self.grid = Gtk.Grid()
         self.overviewGrid = Gtk.Grid()
@@ -42,13 +46,16 @@ class Overview():
         self.build_header()
         self.build_overview()    
         self.display_info(self.data.incomeMenu, self.data.income)
+        self.empty_row()
         self.display_info(self.data.expenseMenu, self.data.expenses)
 
     def build_header(self):
         # Build Header Grid
+        self.blankLabel = Gtk.Label()
+
         self.balanceLabel = Gtk.Label("Balance:  ")
         self.balanceLabel.set_halign(Gtk.Align.END)
-        self.balanceTotalLabel = Gtk.Label( "$" + str(self.sumData(self.data.income)))
+        self.balanceTotalLabel = Gtk.Label( "$" + str(self.sumData(self.data.income) - self.sumData(self.data.expenses)))
         self.balanceTotalLabel.set_halign(Gtk.Align.START)
 
         self.varianceLabel = Gtk.Label("Variance:  ")
@@ -66,6 +73,8 @@ class Overview():
         self.expensesTotalValueLabel = Gtk.Label( "$" + str(self.sumData(self.data.expenses)))
         self.expensesTotalValueLabel.set_halign(Gtk.Align.START)
         
+        self.headerGrid.attach(self.blankLabel,0,0,5,1)
+
         self.headerGrid.attach(self.balanceLabel,0,1,1,1)
         self.headerGrid.attach(self.balanceTotalLabel,1,1,1,1)
         
@@ -106,21 +115,31 @@ class Overview():
         self.overviewGrid.attach(self.contentScrolledWindow,1,1,1,1)
         
         # Print out Months
-        for index in range(1,len(self.data.allMonthMenu)):
-            self.button = Gtk.Button(self.data.allMonthMenu[index][1])
-            self.button.set_relief(Gtk.ReliefStyle.NONE)
-            self.button.set_property("height-request", 30)
-            self.button.set_property("width-request", 120)
-            self.monthGrid.attach(self.button,index,0,1,1)
-            self.monthArr.append([index, self.button])
-            self.button.connect("clicked", self.month_clicked, index)
+        for index in range(1,len(self.data.allMonthMenu) + 1):
+            # Total Header
+            if index == len(self.data.allMonthMenu):
+                self.button = Gtk.Button("Total")
+                self.button.set_relief(Gtk.ReliefStyle.NONE)
+                self.button.set_property("height-request", 30)
+                self.button.set_property("width-request", 120)
+                self.monthGrid.attach(self.button,index,0,1,1)
+                self.monthArr.append([index, self.button])
+                self.button.connect("clicked", self.month_clicked, 13)
+            else:
+                self.button = Gtk.Button(self.data.allMonthMenu[index][1])
+                self.button.set_relief(Gtk.ReliefStyle.NONE)
+                self.button.set_property("height-request", 30)
+                self.button.set_property("width-request", 120)
+                self.monthGrid.attach(self.button,index,0,1,1)
+                self.monthArr.append([index, self.button])
+                self.button.connect("clicked", self.month_clicked, index)
 
         # Style Overview Grid
-        self.clearButton.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.5, .5, .5, .5));
+        self.clearButton.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.menuColor, self.menuColor, self.menuColor, self.menuColor))
         #self.clearButton.set_relief(Gtk.ReliefStyle.NONE)
         
         self.categoryGrid.set_column_homogeneous(True)
-        self.categoryGrid.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.5, .5, .5, .5));
+        self.categoryGrid.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.menuColor, self.menuColor, self.menuColor, self.menuColor))
         
         self.categoryScrolledWindow.set_vexpand(True)
         self.categoryScrolledWindow.set_property("width-request",150)
@@ -133,8 +152,7 @@ class Overview():
         
         self.monthGrid.set_column_homogeneous(True)
         self.monthGrid.set_hexpand(True)
-        self.monthGrid.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.5, .5, .5, .5));
-        
+        self.monthGrid.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.menuColor, self.menuColor, self.menuColor, self.menuColor))        
         self.monthScrolledWindow.set_property("vscrollbar-policy",Gtk.PolicyType.NEVER)
         self.monthScrolledWindow.set_hadjustment(self.contentScrolledWindow.get_hadjustment())
         
@@ -157,17 +175,30 @@ class Overview():
             self.categoryGrid.attach(self.button, 0, self.index, 1, 1)
             
             # Print out total values for each category for each month
-            for month in range(1,len(self.data.allMonthMenu)):
+            for month in range(1,len(self.data.allMonthMenu) + 1):
                 self.total = 0
-                for data in range(0,len(data_arr)):
-                    if data_arr[data][0][0] == index:
-                        if data_arr[data][1][0] == self.data.allMonthMenu[month][0]:
+                # Total Column
+                if month == len(self.data.allMonthMenu):
+                    for data in range(0,len(data_arr)):
+                        if data_arr[data][0][0] == index:
                             self.total += Decimal(data_arr[data][2])
-                self.totalLabel = Gtk.Label("$" + str(self.total))
-                self.totalLabel.set_property("height-request", 40)
-                self.totalLabel.set_property("width-request", 120)
-                self.contentArr.append(self.totalLabel)
-                self.contentGrid.attach(self.totalLabel, month - 1, self.index, 1, 1) 
+                    self.totalLabel = Gtk.Label()
+                    self.totalLabel.set_markup("<b>$" + str(self.total) + "</b>")
+                    self.totalLabel.set_property("height-request", 40)
+                    self.totalLabel.set_property("width-request", 120)
+                    self.totalLabel.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
+                    self.contentArr.append(self.totalLabel)
+                    self.contentGrid.attach(self.totalLabel, month - 1, self.index, 1, 1) 
+                else:
+                    for data in range(0,len(data_arr)):
+                        if data_arr[data][0][0] == index:
+                            if data_arr[data][1][0] == self.data.allMonthMenu[month][0]:
+                                self.total += Decimal(data_arr[data][2])
+                    self.totalLabel = Gtk.Label("$" + str(self.total))
+                    self.totalLabel.set_property("height-request", 40)
+                    self.totalLabel.set_property("width-request", 120)
+                    self.contentArr.append(self.totalLabel)
+                    self.contentGrid.attach(self.totalLabel, month - 1, self.index, 1, 1) 
             self.entryRows.append([self.index, self.contentArr])
             self.index += 1
         
@@ -181,18 +212,32 @@ class Overview():
 
         # Print out total values of all categories for each month 
         self.contentArr = []
-        for month in range(1,len(self.data.allMonthMenu)):
+        for month in range(1,len(self.data.allMonthMenu) + 1):
             self.total = 0
-            for data in range(0,len(data_arr)):
-                if data_arr[data][1][0] == self.data.allMonthMenu[month][0]:
+            # Total Label
+            if month == len(self.data.allMonthMenu):
+                for data in range(0,len(data_arr)):
                     self.total += Decimal(data_arr[data][2])
-            self.totalLabel = Gtk.Label("$" + str(self.total))
-            self.totalLabel.set_property("height-request", 40)
-            self.contentArr.append(self.totalLabel)
-            self.contentGrid.attach(self.totalLabel, month - 1, self.index, 1, 1) 
+                self.totalLabel = Gtk.Label()
+                self.totalLabel.set_markup("<b>$" + str(self.total) + "</b>")
+                self.totalLabel.set_property("height-request", 40)
+                self.totalLabel.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
+                self.contentArr.append(self.totalLabel)
+                self.contentGrid.attach(self.totalLabel, month - 1, self.index, 1, 1) 
+            else:
+                for data in range(0,len(data_arr)):
+                    if data_arr[data][1][0] == self.data.allMonthMenu[month][0]:
+                        self.total += Decimal(data_arr[data][2])
+                self.totalLabel = Gtk.Label()
+                self.totalLabel.set_markup("<b>$" + str(self.total) + "</b>")
+                self.totalLabel.set_property("height-request", 40)
+                self.totalLabel.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
+                self.contentArr.append(self.totalLabel)
+                self.contentGrid.attach(self.totalLabel, month - 1, self.index, 1, 1) 
         self.entryRows.append([self.index, self.contentArr])
         self.index += 1
        
+    def empty_row(self):
         # Set up empty Row
         self.contentArr = []
         self.dummyCategoryButton = Gtk.Button()
@@ -201,9 +246,11 @@ class Overview():
         self.dummyCategoryButton.set_sensitive(False) 
         self.categoryArr.append([self.index, self.dummyCategoryButton])
        
-        for month in range(1,len(self.data.allMonthMenu)):
+        for month in range(1,len(self.data.allMonthMenu) + 1):
             self.dummyContentLabel = Gtk.Label()
             self.dummyContentLabel.set_property("height-request", 30)
+            if month == len(self.data.allMonthMenu):
+                self.dummyContentLabel.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
             self.contentArr.append(self.dummyContentLabel)
             self.contentGrid.attach(self.dummyContentLabel, month - 1, self.index, 1, 1) 
         self.entryRows.append([self.index, self.contentArr])
@@ -218,16 +265,16 @@ class Overview():
                 self.monthArr[i][1].set_relief(Gtk.ReliefStyle.HALF)
                 for j in range(0, len(self.entryRows)):
                     if self.entryRows[j][0] == self.categoryIndex:
-                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.5, .5, .5, .5));
+                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.menuColor, self.menuColor, self.menuColor, self.menuColor))
                     else:
-                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.1, .1, .1, .1));
+                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.highlightColor, self.highlightColor, self.highlightColor, self.highlightColor))
             else:
                 self.monthArr[i][1].set_relief(Gtk.ReliefStyle.NONE)
                 for j in range(0, len(self.entryRows)):
                     if self.entryRows[j][0] != self.categoryIndex:
                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.0, .0, .0, .0));
                     else:
-                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.1, .1, .1, .1));
+                        self.entryRows[j][1][i].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.highlightColor, self.highlightColor, self.highlightColor, self.highlightColor))
     
     def category_clicked(self, button, index):
         self.categoryIndex = index
@@ -236,16 +283,16 @@ class Overview():
                 self.categoryArr[i][1].set_relief(Gtk.ReliefStyle.HALF)
                 for j in range(0, len(self.entryRows[i][1])):
                     if j == self.monthIndex:
-                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.5, .5, .5, .5));
+                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.menuColor, self.menuColor, self.menuColor, self.menuColor))
                     else:
-                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.1, .1, .1, .1));
+                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.highlightColor, self.highlightColor, self.highlightColor, self.highlightColor))
             else:
                 self.categoryArr[i][1].set_relief(Gtk.ReliefStyle.NONE)
                 for j in range(0, len(self.entryRows[i][1])):
                     if j != self.monthIndex:
                         self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.0, .0, .0, .0));
                     else:
-                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.1, .1, .1, .1));
+                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.highlightColor, self.highlightColor, self.highlightColor, self.highlightColor))
     
     def clear_selection(self, button):
         self.categoryIndex = 10000
@@ -256,7 +303,15 @@ class Overview():
             self.monthArr[i][1].set_relief(Gtk.ReliefStyle.NONE)
         for i in range(0, len(self.entryRows)):
             for j in range(0, len(self.entryRows[i][1])):
-                self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.0, .0, .0, .0));
+                if self.entryRows[i][0] == self.categoryArr[len(self.data.incomeMenu) - 1][0]:
+                    self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
+                elif self.entryRows[i][0] == self.categoryArr[len(self.data.incomeMenu) + len(self.data.expenseMenu)][0]:
+                    self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
+                else:
+                    if j == len(self.data.allMonthMenu) - 1:
+                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(self.totalColor, self.totalColor, self.totalColor, self.totalColor))
+                    else:
+                        self.entryRows[i][1][j].override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(.0, .0, .0, .0));
     
     def sumData(self,data_arr):
         total = 0
