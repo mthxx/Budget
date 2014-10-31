435a891b91797e12efd561ca6f102a399a416be1
Marc Thomas <mat@mthx.org>
2014-10-31 14:16:31 -0500

    Improving look of entry's

--- income.py
+++ income.py
@@ -8,31 +8,11 @@ class Income():
         # Define Sidebar Menu
         self.data = Data() 
         self.view = Sidebar() 
-        self.index = 0
+        self.index = 3
         
         self.view.topLeftLabel.set_markup("<b>All Income</b>")
         self.view.topMiddleLabel.set_markup("<b>All Remaining</b>")
         self.view.topRightLabel.set_markup("<b>% Remaining</b>")
-        
-        # Define Layouts
-        self.grid = Gtk.Grid()
-        
-        self.contentGrid = Gtk.Grid()
-        self.contentScrolledWindow = Gtk.ScrolledWindow()
-        self.contentViewport = Gtk.Viewport()
-        
-        self.contentViewport.add(self.contentGrid)
-        self.contentScrolledWindow.add(self.contentViewport)
-        self.grid.attach(self.contentScrolledWindow,0,0,1,1)
-        
-        self.whiteSpaceLabel = Gtk.Label()
-
-        self.grid.attach(self.whiteSpaceLabel,0,0,5,1)
-
-        # Style Layouts
-        self.contentScrolledWindow.set_vexpand(True)
-        self.contentGrid.set_column_homogeneous(True)
-        self.contentGrid.set_hexpand(True)
         
         #Build Content Area
         for i in range (0,len(self.data.income)):
@@ -41,29 +21,35 @@ class Income():
             self.layoutGrid.set_column_homogeneous(True)
             self.layoutGrid.set_hexpand(True)
             
-            self.emptyLabel = Gtk.Label()
+            self.whiteSpaceLabel = Gtk.Label()
             self.index = self.index + 2
-            self.contentGrid.attach(self.emptyLabel,0, self.index, 5, 1)
-
+            self.view.contentGrid.attach(self.whiteSpaceLabel,0, self.index, 5, 1)
             self.index = self.index + 3
             
             self.dateString = ""
             self.dateString = Data.translate_date(self.dateString,self.data.income, i)
 
-            self.categoryLabel = Gtk.Label(self.data.income[i][0][1])
+            self.categoryLabel = Gtk.Label()
+            self.categoryLabel.set_markup("<b>" + self.data.income[i][0][1] + "</b>")
             self.dateLabel = Gtk.Label(self.dateString)
             self.costLabel = Gtk.Label("$" + self.data.income[i][2])
             self.descriptionLabel = Gtk.Label(self.data.income[i][3])
+            #self.whiteSpaceLabel = Gtk.Label()
             
             self.costLabel.set_property("height-request", 35)
             
-            self.layoutGrid.attach(self.categoryLabel, 0, 0, 1, 1)
-            self.layoutGrid.attach(self.dateLabel, 1, 0, 1, 1)
-            self.layoutGrid.attach(self.costLabel, 0, 1, 1, 1)
-            self.layoutGrid.attach(self.descriptionLabel, 1, 1, 1, 1)
+            #self.layoutGrid.attach(self.whiteSpaceLabel,0, 1, 3, 1)
+            self.layoutGrid.attach(self.categoryLabel, 0, 1, 1, 1)
+            self.layoutGrid.attach(self.dateLabel, 1, 1, 1, 1)
+            self.layoutGrid.attach(self.costLabel, 2, 1, 1, 1)
+            
+            if self.descriptionLabel.get_text() != "":
+                self.layoutGrid.attach(self.descriptionLabel, 0, 3, 3, 1)
+                self.whiteSpaceLabel = Gtk.Label()
+                self.layoutGrid.attach(self.whiteSpaceLabel,0, 4, 3, 1)
 
             self.layoutGrid.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
-            self.view.contentGrid.attach(self.layoutGrid, 2, self.index + 3, 3, 2)
+            self.view.contentGrid.attach(self.layoutGrid, 1, self.index, 3, 2)
             self.view.entryRows.append([self.categoryLabel,self.dateLabel,self.costLabel,self.descriptionLabel])
         
         #for i in range (0,len(self.data.income)):
