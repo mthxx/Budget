c22603f5fe5beccc67824a0fd34a30b54c27eacd
Marc Thomas <mat@mthx.org>
2014-10-31 14:18:31 -0500

    Removed header titles

--- sidebar.py
+++ sidebar.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk
 from data import Data
 
 class Sidebar():
@@ -35,11 +35,6 @@ class Sidebar():
         self.contentViewport = Gtk.Viewport()
 
         # Define Widgets
-        self.categoryTitleLabel = Gtk.Label("Category")
-        self.dateTitleLabel = Gtk.Label("Date")
-        self.costTitleLabel = Gtk.Label("Cost")
-        self.descriptionTitleLabel = Gtk.Label("Description")
-        
         self.dummyLabel1 = Gtk.Label()
         self.dummyLabel2 = Gtk.Label()
         
@@ -57,6 +52,7 @@ class Sidebar():
         self.percBudgetTotalLabel = Gtk.Label("50.00%")
 
         # Set Styling
+#        self.grid.override_backgrounddcolor(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.2, 0.2, 0.2, 0.2))
         self.menuScrolledWindow.set_vexpand(True)
         self.menuScrolledWindow.set_property("width-request",150)
         
@@ -65,11 +61,6 @@ class Sidebar():
         
         self.contentGrid.set_column_homogeneous(True)
         self.contentGrid.set_hexpand(True)
-        
-        self.categoryTitleLabel.set_markup("<b>Category</b>")
-        self.dateTitleLabel.set_markup("<b>Date</b>")
-        self.costTitleLabel.set_markup("<b>Cost</b>")
-        self.descriptionTitleLabel.set_markup("<b>Description</b>")
         
         # Add items to Main Grid 
         self.menuViewport.add(self.menuListBox)
@@ -84,23 +75,18 @@ class Sidebar():
         self.grid.attach(self.contentScrolledWindow,1,0,1,1)
 
         # Build Content Area - Add items to Content Grid
-        self.contentGrid.attach(self.topLeftLabel, self.dateOffsetLeft, 2, 1, 1)
-        self.contentGrid.attach(self.topMiddleLabel, self.costOffsetLeft, 2, 1, 1)
-        self.contentGrid.attach(self.topRightLabel, self.descriptionOffsetLeft, 2, 1, 1)
+        self.contentGrid.attach(self.topLeftLabel, 1, 2, 1, 1)
+        self.contentGrid.attach(self.topMiddleLabel, 2, 2, 1, 1)
+        self.contentGrid.attach(self.topRightLabel, 3, 2, 1, 1)
         
-        self.contentGrid.attach(self.monthSpentTotalLabel, self.dateOffsetLeft, 3, 1, 1)
-        self.contentGrid.attach(self.monthRemainingTotalLabel, self.costOffsetLeft, 3, 1, 1)
-        self.contentGrid.attach(self.percBudgetTotalLabel, self.descriptionOffsetLeft, 3, 1, 1)
+        self.contentGrid.attach(self.monthSpentTotalLabel, 1, 3, 1, 1)
+        self.contentGrid.attach(self.monthRemainingTotalLabel, 2, 3, 1, 1)
+        self.contentGrid.attach(self.percBudgetTotalLabel, 3, 3, 1, 1)
         
-        self.contentGrid.attach(self.dummyLabel1, 1, 4, 5, 1)
-        self.contentGrid.attach(self.addEntryButton, 2, 5, 1, 1)
-        self.contentGrid.attach(self.editEntryButton, 4, 5, 1, 1)
+        self.contentGrid.attach(self.dummyLabel1, 0, 4, 3, 1)
+        self.contentGrid.attach(self.addEntryButton, 1, 5, 1, 1)
+        self.contentGrid.attach(self.editEntryButton, 3, 5, 1, 1)
         self.contentGrid.attach(self.dummyLabel2, 1, 6, 1, 1)
-        
-        self.contentGrid.attach(self.categoryTitleLabel, self.categoryOffsetLeft, 7, 1, 1)
-        self.contentGrid.attach(self.dateTitleLabel, self.dateOffsetLeft, 7, 1, 1)
-        self.contentGrid.attach(self.costTitleLabel, self.costOffsetLeft, 7, 1, 1)
-        self.contentGrid.attach(self.descriptionTitleLabel, self.descriptionOffsetLeft, 7, 1, 1)
         
         self.contentViewport.add(self.contentGrid)
 
@@ -122,19 +108,19 @@ class Sidebar():
             self.month =  self.month[0]
             if self.menu == dataMenu[0][1]:
                 if self.subMenu == self.data.currentMonthMenu[0][1]:
-                    self.categoryTitleLabel.show()
+                    #self.categoryTitleLabel.show()
                     self.entryRows[i][0].show()
                     self.entryRows[i][1].show()
                     self.entryRows[i][2].show()
                     self.entryRows[i][3].show()
                 elif self.month == self.subMenu:
-                    self.categoryTitleLabel.show()
+                    #self.categoryTitleLabel.show()
                     self.entryRows[i][0].show()
                     self.entryRows[i][1].show()
                     self.entryRows[i][2].show()
                     self.entryRows[i][3].show()
                 elif self.month != self.subMenu:
-                    self.categoryTitleLabel.show()
+                    #self.categoryTitleLabel.show()
                     self.entryRows[i][0].hide()
                     self.entryRows[i][1].hide()
                     self.entryRows[i][2].hide()
@@ -142,7 +128,7 @@ class Sidebar():
             elif self.menu != dataMenu[0][1]:
                 if self.entryRows[i][0].get_label() == self.menu:
                     if self.subMenu == self.data.currentMonthMenu[0][1]:
-                        self.categoryTitleLabel.hide()
+                        #self.categoryTitleLabel.hide()
                         self.entryRows[i][0].hide()
                         self.entryRows[i][1].show()
                         self.entryRows[i][2].show()
