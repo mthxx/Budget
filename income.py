0b4004ed5a65e7df5a363028d372a710ef6f8b83
Marc Thomas <mat@mthx.org>
2014-10-31 10:15:18 -0500

    Merging new look into income tab for testing and development

--- income.py
+++ income.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk
 from sidebar import Sidebar
 from data import Data
 
@@ -8,13 +8,45 @@ class Income():
         # Define Sidebar Menu
         self.data = Data() 
         self.view = Sidebar() 
+        self.index = 0
         
         self.view.topLeftLabel.set_markup("<b>All Income</b>")
         self.view.topMiddleLabel.set_markup("<b>All Remaining</b>")
         self.view.topRightLabel.set_markup("<b>% Remaining</b>")
         
+        # Define Layouts
+        self.grid = Gtk.Grid()
+        
+        self.contentGrid = Gtk.Grid()
+        self.contentScrolledWindow = Gtk.ScrolledWindow()
+        self.contentViewport = Gtk.Viewport()
+        
+        self.contentViewport.add(self.contentGrid)
+        self.contentScrolledWindow.add(self.contentViewport)
+        self.grid.attach(self.contentScrolledWindow,0,0,1,1)
+        
+        self.whiteSpaceLabel = Gtk.Label()
+
+        self.grid.attach(self.whiteSpaceLabel,0,0,5,1)
+
+        # Style Layouts
+        self.contentScrolledWindow.set_vexpand(True)
+        self.contentGrid.set_column_homogeneous(True)
+        self.contentGrid.set_hexpand(True)
+        
         #Build Content Area
         for i in range (0,len(self.data.income)):
+            
+            self.layoutGrid = Gtk.Grid(name="layoutGrid")
+            self.layoutGrid.set_column_homogeneous(True)
+            self.layoutGrid.set_hexpand(True)
+            
+            self.emptyLabel = Gtk.Label()
+            self.index = self.index + 2
+            self.contentGrid.attach(self.emptyLabel,0, self.index, 5, 1)
+
+            self.index = self.index + 3
+            
             self.dateString = ""
             self.dateString = Data.translate_date(self.dateString,self.data.income, i)
 
@@ -25,12 +57,32 @@ class Income():
             
             self.costLabel.set_property("height-request", 35)
             
-            self.view.contentGrid.attach(self.categoryLabel, self.view.categoryOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
-            self.view.contentGrid.attach(self.dateLabel, self.view.dateOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
-            self.view.contentGrid.attach(self.costLabel, self.view.costOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
-            self.view.contentGrid.attach(self.descriptionLabel, self.view.descriptionOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
-            
+            self.layoutGrid.attach(self.categoryLabel, 0, 0, 1, 1)
+            self.layoutGrid.attach(self.dateLabel, 1, 0, 1, 1)
+            self.layoutGrid.attach(self.costLabel, 0, 1, 1, 1)
+            self.layoutGrid.attach(self.descriptionLabel, 1, 1, 1, 1)
+
+            self.layoutGrid.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
+            self.view.contentGrid.attach(self.layoutGrid, 2, self.index + 3, 3, 2)
             self.view.entryRows.append([self.categoryLabel,self.dateLabel,self.costLabel,self.descriptionLabel])
+        
+        #for i in range (0,len(self.data.income)):
+        #    self.dateString = ""
+        #    self.dateString = Data.translate_date(self.dateString,self.data.income, i)
+#
+#            self.categoryLabel = Gtk.Label(self.data.income[i][0][1])
+#            self.dateLabel = Gtk.Label(self.dateString)
+#            self.costLabel = Gtk.Label("$" + self.data.income[i][2])
+#            self.descriptionLabel = Gtk.Label(self.data.income[i][3])
+#            
+#            self.costLabel.set_property("height-request", 35)
+#            
+#            self.view.contentGrid.attach(self.categoryLabel, self.view.categoryOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
+#            self.view.contentGrid.attach(self.dateLabel, self.view.dateOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
+#            self.view.contentGrid.attach(self.costLabel, self.view.costOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
+#            self.view.contentGrid.attach(self.descriptionLabel, self.view.descriptionOffsetLeft, self.view.entryOffsetTop + i, 1, 1)
+#            
+#            self.view.entryRows.append([self.categoryLabel,self.dateLabel,self.costLabel,self.descriptionLabel])
         
         # Build Sidebars
         for i in range(0,len(self.data.incomeMenu)):
