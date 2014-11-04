ecc952372650d0c141b35a0e5ebe3eddd4d68496
Marc Thomas <mathomastech@gmail.com>
2014-11-03 20:29:36 -0600

    Pointed calculations at calc.py

--- income.py
+++ income.py
@@ -1,19 +1,21 @@
 from gi.repository import Gtk, Gio, Gdk
 from sidebar import Sidebar
 from data import Data
+from calc import Calc
 
 class Income():
 
     def __init__(self):
         # Define Sidebar Menu
         self.data = Data() 
+        self.calc = Calc()
         self.view = Sidebar() 
         
         self.view.topLeftLabel.set_markup("<b>All Income</b>")
         self.view.topMiddleLabel.set_markup("<b>All Remaining</b>")
         self.view.topRightLabel.set_markup("<b>% Remaining</b>")
         
-        self.view.monthTotalLabel.set_text("$" + str(self.view.sumTotalData(self.data.income)))
+        self.view.monthTotalLabel.set_text("$" + str(self.calc.sumTotalData(self.data.income)))
 
         self.view.generate_sidebars(self.data.incomeMenu)
         self.view.generate_content(self.data.income)
