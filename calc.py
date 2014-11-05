f945cbba1ec7b52e08ebc933e22fb9e7e6bffa04
Marc Thomas <mat@mthx.org>
2014-11-05 15:40:52 -0600

    Added calculation for combined category and month

--- calc.py
+++ calc.py
@@ -13,9 +13,23 @@ class Calc():
             total += Decimal(data_arr[i][self.data.value])
         return total
     
+    def sumCategoryData(self,data_arr, category):
+        total = 0
+        for i in range (0,len(data_arr)):
+            if data_arr[i][self.data.category][self.data.category_index] == category:
+                total += Decimal(data_arr[i][self.data.value])
+        return total
+    
     def sumMonthData(self,data_arr, month):
         total = 0
         for i in range (0,len(data_arr)):
             if data_arr[i][self.data.date][self.data.date_month] == month:
+                total += Decimal(data_arr[i][self.data.value])
+        return total
+    
+    def sumCategoryMonthData(self,data_arr, category, month):
+        total = 0
+        for i in range (0,len(data_arr)):
+            if data_arr[i][self.data.category][self.data.category_index] == category and data_arr[i][self.data.date][self.data.date_month] == month:
                 total += Decimal(data_arr[i][self.data.value])
         return total
