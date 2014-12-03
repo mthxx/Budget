0253b6fa031f77bd5117975523193c28007fbcf3
Marc Thomas <mat@mthx.org>
2014-12-03 13:39:29 -0600

    Added support for ctrl+q for quiting

--- Budget.py
+++ Budget.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk
 from window import Window
 from data import Data
 
@@ -10,7 +10,13 @@ class main():
         self.win = Window(self.data)
         self.win.connect("delete-event", Gtk.main_quit)
         self.win.show_all()
+        self.win.connect('key-press-event', self.on_key_function)
         Gtk.main()
+
+    def on_key_function(self, wodget, event):
+        if Gdk.ModifierType.CONTROL_MASK & event.state:# & event.keyval == 113:
+            if event.keyval == 113:
+                Gtk.main_quit()
 
 if __name__=='__main__':
     main()
