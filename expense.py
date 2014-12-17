7e2fa006615b864a79bbe3533d5657a0b59b0a26
Marc Thomas <mat@mthx.org>
2014-12-17 14:13:42 -0600

    Removed expense.py

--- expense.py
+++
@@ -1,8 +0,0 @@
-from sidebar import Sidebar
-
-class Expense():
-
-    def __init__(self, data):
-        # Define Sidebar Menu
-        self.data = data
-        self.view = Sidebar(self.data, "expense") 
