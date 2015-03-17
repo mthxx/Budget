b9844e5c1dd24cf85d36908cf2a9dcece2c66c34
Marc Thomas <mat@mthx.org>
2015-03-16 20:47:54 -0500

    Updated some build information

--- src/setup.py
+++ src/setup.py
@@ -3,27 +3,13 @@ setup(
     name = "Budget",
     version = "0.1",
     packages = find_packages(),
-    scripts = ['Budget.py'],
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    # install_requires = ['docutils>=0.3'],
-
-    #package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-    #    '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-    #    'hello': ['*.msg'],
-    #},
-
-    # metadata for upload to PyPI
+    scripts = ['budget'],
+    install_requires = ['gtk3'],
     author = "Marc Thomas",
-    author_email = "mat@mthx.org",
+    author_email = "mat at mthx dot org",
     description = "Personal Finance",
     license = "GPL v2.0",
     keywords = "budget finance mthx money",
-    url = "http://github.com/mthx_/budget"   # project home page, if any
-
-    # could also include long_description, download_url, classifiers, etc.
+    url = "http://github.com/mthxx/budget"
 )
 
